aae99a0c4979a01471b1ede5a3c7d99e57c07d1f
JuniorCundari <jrcundari@yahoo.com.br>
2022-09-13 14:43:21 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/ads', (request, response) => {
+  return response.json([
+    {
+      id: 1,
+      name: 'anúncio 1'
+    },
+    {
+      id: 2,
+      name: 'anúncio 2'
+    },
+    {
+      id: 3,
+      name: 'anúncio 3'
+    },
+    {
+      id: 4,
+      name: 'anúncio 4'
+    }
+  ])
+})
+
+app.listen(3333)
